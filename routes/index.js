f35f84099be87d0d703ba35b94954f453c1f65ca
Saurabh <saurabh.verma2892@gmail.com>
2018-07-18 16:22:23 -0700

    all good

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,71 @@
 var express = require('express');
 var router = express.Router();
+let request = require("request");
+
 
 /* GET home page. */
+
+router.get('/test', function(req,res,next){
+
+
+    request.post({
+      url: 'http://marcanet.impi.gob.mx/marcanet/vistas/common/dashboard/marcanetDashboardBusquedas.pgi',
+      form: {
+        'frmBsqExp': 'frmBsqExp',
+        'frmBsqExp:busquedaId2': 'frmBsqExp:busquedaId2',
+        'frmBsqExp:expedienteId':'163766',
+        'javax.faces.ViewState':'8301388994051642504:-2333538308694143700',
+        'javax.faces.partial.ajax':'true',
+        'javax.faces.partial.execute':'@all',
+        'javax.faces.partial.render':'frmBsqExp:pnlBsqExp frmBsqExp:dlgListaExpedientes',
+        'javax.faces.source':'frmBsqExp:busquedaId2'
+      }
+    }, function (err, httpResponse, body) { 
+        console.log(body);
+
+        console.log(body.indexOf("redirect url"));
+        console.log(body[69 + 14]);
+        console.log(body.indexOf('</redirect>')-2);
+
+        var mySubString = body.substring(
+            body.indexOf("redirect url")+14, 
+            body.indexOf("</redirect>")-2
+        );
+
+        //request.get("http:/"+mySubString)
+        console.log("http://marcanet.impi.gob.mx"+mySubString);
+
+        request.get("http://marcanet.impi.gob.mx"+mySubString,{},function(err,res2,body){
+          if(err){
+            console.log(err);
+          } 
+          console.log(body);
+          
+
+          
+          var string = body.substring(
+            body.indexOf("dataApoEmailId"), 
+            body.length
+            );
+
+          console.log(string.search("</span></td>"));
+          string = string.substring(
+            16,string.search("</span></td>")
+
+            )
+
+          //res.send(string);
+          res.send(body);
+
+        });
+
+    })
+
+    
+
+
+});
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
